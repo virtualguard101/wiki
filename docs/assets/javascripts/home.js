b72ebb97dc2e210501b39ffd26fe155befa8aded
VIRTUALGUARD <virtualguard101@gmail.com>
2025-10-09 23:47:03 +0800

    chore: add some SaaS

--- docs/assets/javascripts/home.js
+++ docs/assets/javascripts/home.js
@@ -1,3 +1,7 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
+
 document.addEventListener('DOMContentLoaded', function() {
   if (window.initializeSakanaWidget) {
     window.initializeSakanaWidget();
