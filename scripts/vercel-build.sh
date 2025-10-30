07cdf7c30d8c20cc2d39fe5ae5af196c72715a78
VIRTUALGUARD <virtualguard101@gmail.com>
2025-10-30 21:27:50 +0800

    chore: update mkdocs-note in order to fix some issue

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -1,12 +1,2 @@
-# Unshallow the git repository to get full commit history
-# This is required for accurate git timestamps in mkdocs-note plugin
-if [ -d .git ]; then
-    echo "Fetching full git history..."
-    git fetch --unshallow || echo "Repository is already complete"
-    git fetch --all
-else
-    echo "Warning: Not a git repository, git timestamps will not be available"
-fi
-
 source env/bin/activate
 mkdocs build --strict -d public
